bedc1e92d481383cc97c528fffecd760c33ffca0
Ronnie <167436841+Iwis19@users.noreply.github.com>
2026-06-29 02:09:45 -0400

    html form

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flasgger import Swagger
-from rag.rag_pipeline import RAGPipeline
-from utils.rag_guardrail import filter_question
 
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+from pathlib import Path
+
+from rag.rag_pipeline import RAGPipeline
+from rag.guardrail import filter_question
 
 """
 goal:
@@ -11,19 +16,33 @@ goal:
 - have a normal page -> regular upload, regular answers, etc
 """
 
+"""
+1. create a form to upload context file
+2. create a form to submit text questions to ask about yourself
+3. implement ui
+"""
+
+UPLOAD_FOLDER = "context"
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = ""
-app.config['MAX_CONTENT_LENGTH'] = 16*1000*1000 # 16 mb
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16*1024*1024 # 16 mb
+app.config['SECRET_KEY'] = 'hahatemporarysecretkey'
+
+rag_pipeline = RAGPipeline()
+
+class UploadFileForm(FlaskForm):
+    file = FileField("file")
+    submit = SubmitField('submit')
 
 Swagger(app)
 
-rag_pipeline = RAGPipeline()
 
 @app.route("/")
 def home():
     return "Hallo, supposed to be home page..."
 
-@app.get("/ask")
+@app.get("/api/ask")
 def ask():
     question = request.args.get("question")
     return rag_pipeline.ask(question=filter_question(question=question))
@@ -34,11 +53,25 @@ def ask_page():
 
 @app.get("/upload")
 def upload_page():
-    return render_template("upload_page.html")
+    form = UploadFileForm()
+    return render_template("upload_page.html", form=form)
 
 @app.post("/upload")
 def upload_file():
-    rag_pipeline.insert_info(["context/profile.txt"])
+    # work on form submission logic
+
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(Path(app.config['UPLOAD_FOLDER']) / secure_filename(filename=file.filename))
+        flash("Successfully uploaded!")
+        return redirect(url_for("ask_page"))
+    
+    flash("Upload failed, please upload a valid file.")
+    return redirect(url_for("upload_page"))     # sends me to the page routed to the "upload_page" method, in this case is @app.get("/upload")
+
+    #rag_pipeline.insert_info(["context/profile.txt"])
+
 
 if __name__ == "__main__":
     app.run(debug=True)
